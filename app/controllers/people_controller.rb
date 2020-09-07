2a1ef5983dd7eec40db00d1eee560173e6373552
Ben Quartermaine <ben.quartermaine@ausmed.com.au>
2020-09-08 06:47:40 +0800

    routes, controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,23 +1,40 @@
 class PeopleController < ApplicationController
+  before_action :find_person, only: [:edit, :show, :update, :destroy]
   def index
     @people = Person.all
   end
   
   def create
+    @person = Person.new(person_params)
+    @person.save
+    redirect_to person_path(@person)
   end
   
   def new
+    @person = Person.new()
   end
 
-  def edit
-  end
+  def edit; end
 
-  def show
-  end
+  def show; end
 
   def update
+    @person.update(person_params)
+    redirect_to person_path(@person)
   end
   
   def destroy
+    @person.destroy
+    redirect_to people_path
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:name, :age, :family_member, :pets)
+  end
+
+  def find_person 
+    @person = Person.find(params[:id])
   end
 end
